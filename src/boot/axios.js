29b88776bb88e5a40e42b8aae2b1a63650195179
Willian <wsilvadev@gmail.com>
2020-12-10 16:29:29 -0300

    feat(boot/axios): create setup to use api

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,10 @@
-import Vue from 'vue'
-import axios from 'axios'
+// import Vue from 'vue';
+import axios from 'axios';
 
-Vue.prototype.$axios = axios
+const HTTPClient = axios.create({
+    baseURL: process.env.API
+});
+
+export { HTTPClient };
+
+// Vue.prototype.$axios = axios;
