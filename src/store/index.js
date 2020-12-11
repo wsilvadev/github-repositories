f0ac02b01f3362c75fac76293483f8629f5b40b8
Willian <wsilvadev@gmail.com>
2020-12-11 15:09:21 -0300

    feat(store): add posts to index

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-// import example from './module-example'
+import Posts from './posts';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 /*
  * If not building with SSR mode, you can
@@ -14,16 +14,16 @@ Vue.use(Vuex)
  * with the Store instance.
  */
 
-export default function (/* { ssrContext } */) {
-  const Store = new Vuex.Store({
-    modules: {
-      // example
-    },
+export default function(/* { ssrContext } */) {
+    const Store = new Vuex.Store({
+        modules: {
+            Posts
+        },
 
-    // enable strict mode (adds overhead!)
-    // for dev mode only
-    strict: process.env.DEBUGGING
-  })
+        // enable strict mode (adds overhead!)
+        // for dev mode only
+        strict: process.env.DEBUGGING
+    });
 
-  return Store
+    return Store;
 }
