9cd4537d59bc758c67ef9372fc5d97823fe2a63d
Willian <wsilvadev@gmail.com>
2020-12-14 18:17:34 -0300

    feat(route): create route to repos

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,24 @@
-
 const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
-  },
+    {
+        path: '/',
+        component: () => import('layouts/MainLayout.vue'),
+        children: [
+            { path: '', component: () => import('pages/Index.vue') },
+            {
+                path: 'repos',
+                name: 'repos',
+                component: () => import('pages/Repos.vue'),
+                params: {}
+            }
+        ]
+    },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  }
-]
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+        path: '*',
+        component: () => import('pages/Error404.vue')
+    }
+];
 
-export default routes
+export default routes;
