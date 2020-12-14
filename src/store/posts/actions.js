63b30ac32f16aed08a0720c60c6e4abc711d9732
Willian <wsilvadev@gmail.com>
2020-12-14 18:15:34 -0300

    feat(posts): create actions

--- src/store/posts/actions.js
+++ src/store/posts/actions.js
@@ -1,4 +1,16 @@
-/*
-export function someAction (context) {
-}
-*/
+import { HTTPClient } from 'boot/axios';
+
+// setPosts  vai estar setando algo no estado..
+
+const setPosts = ({ commit }, userName) => {
+    return new Promise((resove, reject) => {
+        HTTPClient.get(`/users/${userName}`)
+            .then(suc => {
+                resove(suc.data);
+                commit('SET_POSTS', suc.data);
+            })
+            .catch(err => reject(err));
+    });
+};
+
+export { setPosts };
