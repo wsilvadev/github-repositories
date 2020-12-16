9861e902821c1c405e1238ec9896fd91f409010b
Willian <wsilvadev@gmail.com>
2020-12-16 16:22:27 -0300

    feat(posts): create actions to get repositories and userLogin

--- src/store/posts/actions.js
+++ src/store/posts/actions.js
@@ -2,9 +2,9 @@ import { HTTPClient } from 'boot/axios';
 
 // setPosts  vai estar setando algo no estado..
 
-const setPosts = ({ commit }, userName) => {
+const setPosts = ({ commit, getters }) => {
     return new Promise((resove, reject) => {
-        HTTPClient.get(`/users/${userName}`)
+        HTTPClient.get('/users/' + getters.inputTextChanged)
             .then(suc => {
                 resove(suc.data);
                 commit('SET_POSTS', suc.data);
@@ -12,5 +12,19 @@ const setPosts = ({ commit }, userName) => {
             .catch(err => reject(err));
     });
 };
+const setRepos = ({ commit }, user) => {
+    return new Promise((resove, reject) => {
+        HTTPClient.get('/users/' + user + '/repos')
+            .then(suc => {
+                resove(suc.data);
+                commit('SET_REPOS', suc.data);
+            })
+            .catch(err => reject(err));
+    });
+};
 
-export { setPosts };
+const setInputText = ({ commit }, text) => {
+    commit('CHANGE_TEXT_INPUT', text);
+};
+
+export { setPosts, setInputText, setRepos };
